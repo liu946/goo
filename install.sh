072c5ad1e00056a308f3913b43767a394f4e93ad
liu946 <liuyang570926881@gmail.com>
2017-05-12 10:47:28 +0800

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ mkdir -p ~/.goo/script
 cp -f $(pwd)/goo.sh ~/.goo/goo.sh
 cp -f $(pwd)/goo.json ~/.goo/goo.json
 cp -f ./script/init.sample.sh ~/.goo/script/init.sh
+cp -f ./script/update.sample.sh ~/.goo/script/update.sh
 
 ln -sf ~/.goo/goo.sh /usr/local/bin/goo
 
---
+++ script/update.sample.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf ~/.goo
+git clone https://github.com/liu946/goo.git goo
+cd goo
+./install.sh
+cd ..
+rm -r goo
